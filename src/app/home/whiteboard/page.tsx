ae1a8c61d67ef38f14e76453b1efb671a9a086b5
alysalah83 <alysalah83@gmail.com>
2026-02-26 21:55:13 +0200

    remove old whiteboard userId prop

--- src/app/home/whiteboard/page.tsx
+++ src/app/home/whiteboard/page.tsx
@@ -1,20 +1,14 @@
-import { authServices } from "@/features/auth/services/auth.service";
 import Whiteboard from "@/features/whiteboard/components/Whiteboard";
 import { Metadata } from "next";
-import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Whiteboard",
 };
 
 async function page() {
-  const user = await authServices.getUser();
-
-  if (!user) return redirect("/login");
-
   return (
     <div className="h-full w-full">
-      <Whiteboard userId={user?.id} />
+      <Whiteboard />
     </div>
   );
 }
