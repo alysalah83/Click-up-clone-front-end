9b33d6d3864e5dc6c84606bccf9a5b6f15555d88
alysalah83 <alysalah83@gmail.com>
2026-02-26 20:26:22 +0200

    fix typing bug in the client endpoints url path

--- src/features/task/api/tasks.client.ts
+++ src/features/task/api/tasks.client.ts
@@ -4,7 +4,7 @@ import { Task } from "../types";
 export async function getTasksClient(listId: Task["id"], filters: string) {
   return await axiosClient.get<Task[]>(
     filters
-      ? `api/tasks?listId=${listId}&${filters}`
-      : `api/tasks?listId=${listId}&createdAt=desc`,
+      ? `/api/tasks?listId=${listId}&${filters}`
+      : `/api/tasks?listId=${listId}&createdAt=desc`,
   );
 }
