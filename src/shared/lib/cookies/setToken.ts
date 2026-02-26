f1df8c26e5e22f7584571c74c86e223bcac68870
alysalah83 <alysalah83@gmail.com>
2026-02-26 18:55:45 +0200

    edditing cookies options for dev and production

--- src/shared/lib/cookies/setToken.ts
+++ src/shared/lib/cookies/setToken.ts
@@ -3,8 +3,8 @@ import { cookies } from "next/headers";
 export const setToken = async (token: string) => {
   (await cookies()).set("token", token, {
     httpOnly: true,
-    secure: true,
-    sameSite: "none",
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     maxAge: 90 * 24 * 60 * 60 * 1000,
     path: "/",
   });
