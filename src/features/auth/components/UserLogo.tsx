e3ca268f1684b15cde8ad52fd88fcc7c2334d467
alysalah83 <alysalah83@gmail.com>
2026-02-26 15:58:18 +0200

    fix UserLogo name proprtie type bug

--- src/features/auth/components/UserLogo.tsx
+++ src/features/auth/components/UserLogo.tsx
@@ -18,10 +18,11 @@ function UserLogo({
       </Link>
     );
   const { name } = user;
-  const nameFirstLetters = name!
-    .split(" ")
-    .map((name) => name[0].toUpperCase())
-    .join("");
+  const nameFirstLetters =
+    name
+      ?.split(" ")
+      .map((name) => name[0].toUpperCase())
+      .join("") ?? "user";
   return (
     <div className="flex cursor-default items-center gap-2 rounded-lg px-2 py-1">
       <div className="flex h-6 w-6 items-center justify-center truncate rounded-full bg-lime-300 text-xs font-medium sm:h-8 sm:w-8 sm:text-sm dark:bg-lime-700">
